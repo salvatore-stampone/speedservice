2427deb1cfe1a8aa67427640041158ed991403fc
Salvatore Stampone <salvatore.stampone@brots.cloud>
2023-02-04 13:06:24 +0100

    small fixed on tracking modal

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,25 +1,22 @@
-import { cloneElement, useState } from "react";
+import { cloneElement } from "react";
 
-export default function Dropdown({ trigger, menu }) {
-    const [open, setOpen] = useState(false);
-
-    const handleOpen = () => {
-        setOpen(!open);
-    };
+export default function Dropdown({ trigger, menu, isDropdownOpen, setIsDropdownOpen }) {
 
     return (
         <div className='relative'>
             {cloneElement(trigger, {
-                onClick: handleOpen,
+                onClick: () => {
+                    setIsDropdownOpen(!isDropdownOpen);
+                },
             })}
-            {open ? (
+            {isDropdownOpen ? (
                 <ul className='absolute list-none my-1.5 rounded-2xl w-36 shadow-2xl'>
                     {menu.map((menuItem, index) => (
                         <li key={index} className='bg-light first-of-type:rounded-b-none first-of-type:rounded-t-2xl last-of-type:rounded-t-none last-of-type:rounded-b-2xl hover:bg-gray-200 w-[200px]'>
                             {cloneElement(menuItem, {
                                 onClick: () => {
                                     menuItem.props.onClick && menuItem.props.onClick();
-                                    setOpen(false);
+                                    setIsDropdownOpen(false);
                                 },
                             })}
                         </li>
--- src/components/Tracking/Float.jsx
+++ src/components/Tracking/Float.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { BsBoxSeam } from "react-icons/bs"
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import Dropdown from '../Dropdown';
 import brt from '../../assets/images/logo-squares/brt-square.jpg'
@@ -9,10 +8,19 @@ import gls from '../../assets/images/logo-squares/gls-square.jpg'
 import sda from '../../assets/images/logo-squares/sda-square.png'
 import tnt from '../../assets/images/logo-squares/tnt-square.png'
 import ups from '../../assets/images/logo-squares/ups-square.png'
+import { IoCloseCircle, IoCloseSharp } from 'react-icons/io5';
 
 export default function Float() {
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const [fade, setFade] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setFade(isModalOpen)
+        }, 0)
+    }, [isModalOpen])
 
     const couriers = [
         {
@@ -49,28 +57,45 @@ export default function Float() {
 
     return (
         <>
-            {isOpen &&
-                <div className='fixed top-[40%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-10 bg-light w-80 h-40 flex items-center justify-center rounded-2xl shadow-2xl lg:w-96'>
+            {isModalOpen &&
+                <div className={`${isModalOpen ? 'flex' : 'hidden'} ${fade ? 'opacity-100' : 'opacity-0'} fixed top-[40%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-10 bg-light w-80 h-40 items-center justify-center rounded-2xl shadow-2xl lg:w-96 transition-opacity duration-500`}>
+                    <IoCloseSharp
+                        className='absolute top-4 right-5 text-xl cursor-pointer hover:text-primary'
+                        onClick={() => {
+                            setIsModalOpen(false)
+                            setIsDropdownOpen(false)
+                        }
+                        } />
                     <Dropdown
                         trigger={
-                            <button className='text-lg hover:text-primary transition-colors lg:text-xl'>
+                            <button className={`text-lg hover:text-primary transition-colors lg:text-xl ${isDropdownOpen ? 'text-primary' : 'text-black'}`}>
                                 Seleziona il tuo corriere
-                                <RiArrowDropDownLine className='inline text-2xl' />
+                                <RiArrowDropDownLine className={`inline text-2xl ${isDropdownOpen && 'rotate-180'} transition-all duration-300`} />
                             </button>}
                         menu={
                             couriers.map((courier) => {
                                 return (
-                                    <a className='w-full h-full text-left border-none p-1.5 cursor-pointer block' href={courier.url} target='_blank' rel='noreferrer'>
-                                        <img src={courier.image} alt={courier.name} className='rounded-full aspect-square w-8 inline-block mr-2' />
+                                    <a
+                                        className='w-full h-full text-left border-none p-1.5 cursor-pointer block'
+                                        href={courier.url}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                        onClick={() => setIsModalOpen(false)}>
+                                        <img
+                                            src={courier.image}
+                                            alt={courier.name}
+                                            className='rounded-full aspect-square w-8 inline-block mr-2' />
                                         {courier.name}
                                     </a>
                                 )
                             })
                         }
+                        isDropdownOpen={isDropdownOpen}
+                        setIsDropdownOpen={setIsDropdownOpen}
                     />
                 </div>}
             <button
-                onClick={() => { setIsOpen(!isOpen) }}
+                onClick={() => { setIsModalOpen(!isModalOpen) }}
                 className="float left-4 bg-primary-dark text-light py-4 px-4 rounded-full z-[2] cursor-pointer"
             >
                 TRACKING
